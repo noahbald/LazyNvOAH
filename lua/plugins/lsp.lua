8448b37c06640d2e77e27c3449d40ed0087aa50f
noahbald <noahwbaldwin@gmail.com>
2023-11-24 23:59:35 +1100

    Add better folds

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -25,4 +25,42 @@ return {
 		"axelvc/template-string.nvim",
 		event = "LspAttach",
 	},
+
+	-- Add folding range capabilities
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			capabilities = {
+				textDocument = {
+					foldingRange = {
+						dynamicRegistration = false,
+						lineFoldingOnly = true,
+					},
+				},
+			},
+		},
+	},
+
+	-- add nvim-ufo
+	{
+		"kevinhwang91/nvim-ufo",
+		dependencies = {
+			"kevinhwang91/promise-async",
+			"luukvbaal/statuscol.nvim",
+		},
+		event = "BufReadPost",
+		opts = function()
+			return require("config.ufo")
+		end,
+
+		init = function()
+			-- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+			vim.keymap.set("n", "zR", function()
+				require("ufo").openAllFolds()
+			end)
+			vim.keymap.set("n", "zM", function()
+				require("ufo").closeAllFolds()
+			end)
+		end,
+	},
 }
