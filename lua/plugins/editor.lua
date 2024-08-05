8fe3a5bcf5e1ff8e017b1f38c9b85749db510fc6
Noah Baldwin <Noah.Baldwin@anz.com>
2024-08-05 16:22:39 +1000

    add git-graph

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -136,4 +136,37 @@ return {
 			start_buf({ buf = 0 })
 		end,
 	},
+
+	-- git graph
+	{
+		"isakbm/gitgraph.nvim",
+		---@type I.GGConfig
+		opts = {
+			symbols = {
+				merge_commit = "M",
+				commit = "*",
+			},
+			format = {
+				timestamp = "%H:%M:%S %d-%m-%Y",
+				fields = { "hash", "timestamp", "author", "branch_name", "tag" },
+			},
+			hooks = {
+				on_select_commit = function(commit)
+					print("selected commit:", commit.hash)
+				end,
+				on_select_range_commit = function(from, to)
+					print("selected range:", from.hash, to.hash)
+				end,
+			},
+		},
+		keys = {
+			{
+				"<leader>gl",
+				function()
+					require("gitgraph").draw({}, { all = true, max_count = 5000 })
+				end,
+				desc = "GitGraph - Draw",
+			},
+		},
+	},
 }
